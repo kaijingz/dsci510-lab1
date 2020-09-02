dee5f99c4761110461deecfb0c164d12558c2a00
KaijingZhang <kaijingzhang@KaijingangdeMBP.attlocal.net>
2020-09-02 16:19:29 -0700

    Add and update two files

--- lab1.py
+++ lab1.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
+def print_func(parameter):
+    print(parameter)
+
+
 # In[2]:
 
 
-print("Welcome to DCSCI510!")
+print_func("Welcome to DCSCI510!")
 
 
 # In[ ]:
